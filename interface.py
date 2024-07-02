dd90f0415369c1f2fe0caba820158f07ce531461
Luiz Albert dos santos machado <luizmachado20010@gmail.com>
2024-07-02 20:36:18 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,13 +2,16 @@ from tkinter import *
 from tkinter import messagebox
 
 class interface():
-    def __init__(self, title, geometry, background):
+    def __init__(self, title="meu titulo", geometry="400x400", background="light blue"):
         self.janela = Tk()
         self.janela.title(title)
         self.janela.geometry(geometry)
         self.janela.configure(bg=background)
+
+    def run(self):
         self.janela.mainloop()
 
 
 
-interface("tela 1", "350x350", "black")
+interface = interface()
+interface.run()
